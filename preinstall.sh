2884bcf9b242d92b884cfcfaafedf88323819bba
Roger <roger@rcod.net>
2021-06-15 10:09:39 +0200

    Bug all volumes with /home/roger/rCod/Projects/tesla-ce/docker-compose

--- preinstall.sh
+++ preinstall.sh
@@ -3,17 +3,17 @@ docker network create tesla_public
 docker network create tesla_private
 
 # Create volume folder structure
-mkdir -p ./_data/db
-mkdir -p ./_data/traefik
-mkdir -p ./_data/rabbitmq
-mkdir -p ./_data/minio
-mkdir -p ./_data/redis
+mkdir -p ${PWD}/_data/db
+mkdir -p ${PWD}/_data/traefik
+mkdir -p ${PWD}/_data/rabbitmq
+mkdir -p ${PWD}/_data/minio
+mkdir -p ${PWD}/_data/redis
 
-mkdir -p ./_data/moodle
-mkdir -p ./_data/database_moodle
+mkdir -p ${PWD}/_data/moodle
+mkdir -p ${PWD}/_data/database_moodle
 
-cp .env_moodle ./config/env_moodle_template
-cp .env_tfr ./config/env_tfr_template
+cp ${PWD}/.env_moodle ${PWD}/config/env_moodle_template
+cp ${PWD}/.env_tfr ${PWD}/config/env_tfr_template
 
 # UP services with configure
 docker-compose -f docker-compose.tesla_services.yml -f docker-compose.configure.yml up configure
